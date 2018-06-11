5e4a8ff3fcc96170b18f67d9fc04a256b90ff25b
WangJianxin <a1bertwang@foxmail.com>
2018-06-11 09:25:36 +0800

    Initial Commit

---
+++ DataStructures/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructures
+//
+//  Created by Albert on 2018/6/11.
+//  Copyright © 2018年 hzblossom. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
